e0440e41c152623ec97791eaa1f82c54aeeca210
Anna Belyaeva <belyaeva13a@gmail.com>
2020-11-30 20:18:14 +0300

    first commit

---
+++ app/src/main/java/com/example/goracing/GameActivity.java
@@ -0,0 +1,104 @@
+package com.example.goracing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameActivity extends AppCompatActivity {
+     private   ImageView iv_pccar, iv_usercar, iv_finishline;
+     private   TextView tv_timer, tv_semafor;
+     Timer timeGame = new Timer();
+     Timer timePC = new Timer();
+     int state = 0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+         iv_pccar = (ImageView) findViewById(R.id.iv_pccar);
+        iv_usercar = (ImageView) findViewById(R.id.iv_usercar);
+        iv_finishline = (ImageView) findViewById(R.id.iv_finishline);
+        tv_timer = (TextView) findViewById(R.id.tv_timer);
+        tv_semafor = (TextView) findViewById(R.id.tv_semafor);
+
+    }
+    public void startGameAction(View view) {
+        timerGame();
+        timerPC();
+    }
+    public void driveCarAction(View view) {
+        if (state == 2) {
+            iv_usercar.setX(iv_usercar.getX() + 30);
+        }
+        if (state == 1) {
+            iv_usercar.setX(iv_usercar.getX() - 30);
+        }
+        if (iv_usercar.getX() >= iv_finishline.getX()) {
+            timePC.cancel();
+            timeGame.cancel();
+            Toast.makeText(getApplicationContext(), "YOU WIN", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void timerGame() {
+        TimerTask timerTask = new TimerTask() {
+
+            @Override
+            public void run() {
+                 runOnUiThread(new Runnable() {
+                     @Override
+                     public void run() {
+                         state += 1;
+                         if (state >2)
+                             state=1;
+                         switch (state){
+                             case 1:
+                                 tv_semafor.setText("RED");
+                                 tv_semafor.setTextColor(Color.RED);
+                                 break;
+                             case 2:
+                                 tv_semafor.setText("GREEN");
+                                 tv_semafor.setTextColor(Color.GREEN);
+                                 break;
+                         }
+                     }
+                 });
+            }
+        };
+        timeGame.scheduleAtFixedRate(timerTask, 0, 3000);
+    }
+    public void timerPC() {
+        Timer timerPC = new Timer();
+        Timer timerGame = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable(){
+
+                    @Override
+                    public void run(){
+                        if (state ==2) {
+                            iv_pccar.setX(iv_pccar.getX() + 300);
+                        }
+                        if (iv_pccar.getX() >= iv_finishline.getX()){
+                            timerPC.cancel();
+                            timerGame.cancel();
+                            Toast.makeText(getApplicationContext(), "YOU LOSE!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        };
+        timerPC.scheduleAtFixedRate(timerTask, 0, 1000);
+    }
+}
